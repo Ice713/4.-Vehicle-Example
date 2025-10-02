9d21c0b14286fc2e74bab897455ccb8b140bc5af
Ice713 <icramzilruiz@gmail.com>
2025-10-02 10:08:18 +0800

    Add project files.

---
+++ 4. Vehicle Example/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Vehicle_Example
+{
+    internal class Vehicle
+    {
+        public string Start()
+        {
+            return "Vehicle started.";
+        }
+    }
+}
